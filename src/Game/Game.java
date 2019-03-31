912937cd3e72cd0f273f88cfe9e1bc75afd55cef
Raizusekku <neupanenischal1@gmail.com>
2019-03-31 15:21:37 -0500

    added a method for score calculation and word validation

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -10,7 +10,6 @@ import Data.GameTimer;
 
 public class Game {
 	private static Board board; //currently static for testing
-	private Dictionary dictionary;
 	private int score;
 	private GameTimer timer;
 	private String currentWord;
@@ -19,6 +18,8 @@ public class Game {
 	public Game(JFrame frame) {
 		board = new Board(frame);
 		Dictionary.generateDictionary();
+		wordList = new ArrayList<>();
+		score = 0;
 	}
 	
 	public static void addLetter(String letter) {
@@ -30,22 +31,45 @@ public class Game {
 		board.randomize();
 	}
 	
-	private String getFormattedList() {
+	public String getFormattedList() {
 		StringBuilder formattedList = new StringBuilder("<html>");
-		for (int i = 0; i < wordList.size() - 1; i++) {
-			formattedList.append(wordList.get(i) + "<br>");
+		if(wordList.size() > 0) {
+			for (int i = 0; i < wordList.size() - 1; i++) {
+				formattedList.append(wordList.get(i) + "<br>");
+			}
+			formattedList.append(wordList.get(wordList.size() - 1) + "</html>");
 		}
-		formattedList.append(wordList.get(wordList.size() - 1) + "</html>");
-		
 		return formattedList.toString();
 	}
 
 	public static void startGame() {
 		
 	}
+
+	public int getScore(){
+		return this.score;
+	}
+
+	public String getLatestWord(){
+		return this.wordList.size() < 1 ? "-" : this.wordList.get(this.wordList.size()-1);
+	}
 	
-	public void submitWord(String word) {
-		
+	public void submitWord() {
+		int length = this.currentWord == null ? 0 : this.currentWord.length();
+		if(length >= 3){
+			if(Dictionary.checkWord(this.currentWord)){
+				switch (length) {
+					case 3 : { score += 1; break; }
+					case 4 : { score += 1; break; }
+					case 5 : { score += 2; break; }
+					case 6 : { score += 3; break; }
+					case 7 : { score += 5; break; }
+					default : { score += 11; break; }
+				}
+				this.wordList.add(this.currentWord);
+			}
+		}
+		currentWord = "";
 	}
 	
 	//test function
