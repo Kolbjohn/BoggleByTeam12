d017fdc4fb53de0e36f9c9cf53bf3b2d850373a4
Raizusekku <neupanenischal1@gmail.com>
2019-04-16 21:12:18 -0500

    updated dictionary to fully work with german words and added few unit tests for german

--- src/Data/Dictionary.java
+++ src/Data/Dictionary.java
@@ -8,29 +8,29 @@ package Data;
 import UI.UI;
 
 import java.io.*;
-import java.text.Collator;
 import java.util.ArrayList;
 
 public class Dictionary {
     // Global arraylist to store all the words
     private static ArrayList<String> wordsInDictionary = new ArrayList<>();
-    final Collator instance = Collator.getInstance();
 
     public Dictionary(){
         generateDictionary();
-        instance.setStrength(Collator.SECONDARY);
     }
 
     // Reads the external dictionary text file and stores them in words list
     private void generateDictionary(){
         InputStream in = UI.isEnglish ? Dictionary.class.getResourceAsStream("dictionary_en.txt") : Dictionary.class.getResourceAsStream("dictionary_de.txt");
+        InputStreamReader is = new InputStreamReader(in);
         try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            BufferedReader br = new BufferedReader(is);
             String currentLine;
             while((currentLine = br.readLine()) != null){
                 wordsInDictionary.add(currentLine.trim());
             }
+            is.close();
             br.close();
+            in.close();
         }
         catch(Exception e){
             e.printStackTrace();
@@ -42,10 +42,10 @@ public class Dictionary {
         int low = 0, mid, high = wordsInDictionary.size() - 1;
         while(low <= high){
             mid = (low + high)/2;
-            if(instance.compare(wordsInDictionary.get(mid), word.toUpperCase()) < 0){
+            if(wordsInDictionary.get(mid).toUpperCase().compareTo(word.toUpperCase()) < 0){
                 low = mid + 1;
             }
-            else if(instance.compare(wordsInDictionary.get(mid), word.toUpperCase()) > 0){
+            else if(wordsInDictionary.get(mid).toUpperCase().compareTo(word.toUpperCase()) > 0){
                 high = mid - 1;
             }
             else{
--- src/test/DictionaryTest.java
+++ src/test/DictionaryTest.java
@@ -1,17 +1,53 @@
 package test;
 
 import Data.Dictionary;
+import UI.UI;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DictionaryTest {
     private Dictionary dictionary = new Dictionary();
 
-    @Test
-    @DisplayName("It checks if it's a word!")
-    void testCheckWord(){
-        assertTrue(dictionary.checkWord("true"));
+    @Nested
+    @DisplayName("Checks for the correct words in each language")
+    class TestCorrectText{
+
+        @Test
+        @DisplayName("Is it a valid English word?")
+        void testEnglishWord(){
+            UI.isEnglish = true;
+            assertTrue(dictionary.checkWord("test"));
+        }
+
+        @Test
+        @DisplayName("Is it a valid German word?")
+        void testGermanWord(){
+            UI.isEnglish = false;
+            assertTrue(dictionary.checkWord("blutungen"));
+        }
     }
+
+    @Nested
+    @DisplayName("Checks for the invalid words in each language")
+    class TestIncorrectText{
+
+        @Test
+        @DisplayName("Does it catch an invalid English word?")
+        void testEnglishWord(){
+            UI.isEnglish = true;
+            assertFalse(dictionary.checkWord("boiii"));
+        }
+
+        @Test
+        @DisplayName("Does it catch an invalid German word?")
+        void testGermanWord(){
+            UI.isEnglish = false;
+            assertFalse(dictionary.checkWord("book"));
+        }
+    }
+
 }
